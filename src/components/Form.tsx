486f6f461da4356390b32832f5046e818528edfb
Elivelton Alves <eliveltonsa92@gmail.com>
2023-11-07 16:59:33 -0300

    feat: melhorias no layout

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -4,11 +4,7 @@ import { User } from "../types/User";
 
 export const Form = () => {
   const [name, setName] = useState("");
-  const [lastname, setLastname] = useState("");
   const [email, setEmail] = useState("");
-  const [gender, setGender] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
   const [agree, setAgree] = useState(false);
 
   const [errors, setErrors] = useState<User | null>(null);
@@ -19,11 +15,7 @@ export const Form = () => {
 
     const data: User = {
       name,
-      lastname,
       email,
-      gender,
-      password,
-      confirmPassword,
       agree,
     };
 
@@ -37,12 +29,9 @@ export const Form = () => {
     console.log(data);
 
     setName("");
-    setLastname("");
     setEmail("");
-    setGender("");
-    setPassword("");
-    setConfirmPassword("");
     setAgree(false);
+    alert("Obrigado por se inscrever");
   };
 
   return (
@@ -63,24 +52,7 @@ export const Form = () => {
           <small className="text-xs text-red-500 mt-1">{errors?.name}</small>
         )}
       </div>
-      <div className="flex flex-col">
-        <label className="text-sm" htmlFor="lastname">
-          Sobenome
-        </label>
-        <input
-          className="rounded-lg focus:ring-0 focus:outline-none py-2 px-2 text-sm placeholder:text-sm placeholder:text-stone-400"
-          type="text"
-          id="lastname"
-          value={lastname}
-          placeholder="Insira seu sobrenome"
-          onChange={(e) => setLastname(e.target.value)}
-        />
-        {errors?.lastname && (
-          <small className="text-xs text-red-500 mt-1">
-            {errors?.lastname}
-          </small>
-        )}
-      </div>
+
       <div className="flex flex-col">
         <label className="text-sm" htmlFor="email">
           E-mail
@@ -97,71 +69,7 @@ export const Form = () => {
           <small className="text-xs text-red-500 mt-1">{errors?.email}</small>
         )}
       </div>
-      <div className="flex flex-col">
-        <label className="text-sm" htmlFor="gender">
-          Gênero
-        </label>
-        <select
-          className="rounded-lg p-1 focus:ring-0 focus:outline-none"
-          id="gender"
-          value={gender}
-          onChange={(e) => setGender(e.target.value)}
-        >
-          <option value="" disabled className="text-sm">
-            Selecione
-          </option>
-          <option value="masculino" className="text-sm">
-            Masculino
-          </option>
-          <option value="feminino" className="text-sm">
-            Feminino
-          </option>
-          <option value="outro" className="text-sm">
-            Outro
-          </option>
-        </select>
-        {errors?.gender && (
-          <small className="text-xs text-red-500 mt-1">{errors?.gender}</small>
-        )}
-      </div>
-      <div className="flex flex-col">
-        <label className="text-sm" htmlFor="password">
-          Senha
-        </label>
-        <input
-          className="rounded-lg focus:ring-0 focus:outline-none py-2 px-2 text-sm placeholder:text-sm placeholder:text-stone-400"
-          type="password"
-          id="password"
-          value={password}
-          placeholder="Insira sua senha"
-          autoComplete="true"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        {errors?.password && (
-          <small className="text-xs text-red-500 mt-1">
-            {errors?.password}
-          </small>
-        )}
-      </div>
-      <div className="flex flex-col">
-        <label className="text-sm" htmlFor="confirmpassword">
-          Confirme sua Senha
-        </label>
-        <input
-          className="rounded-lg focus:ring-0 focus:outline-none py-2 px-2 text-sm placeholder:text-sm placeholder:text-stone-400"
-          type="password"
-          id="confirmpassword"
-          value={confirmPassword}
-          placeholder="Confirme sua senha"
-          autoComplete="true"
-          onChange={(e) => setConfirmPassword(e.target.value)}
-        />
-        {errors?.confirmPassword && (
-          <small className="text-xs text-red-500 mt-1">
-            {errors?.confirmPassword}
-          </small>
-        )}
-      </div>
+
       <div className="flex flex-col">
         <a href="#" className="text-xs underline mb-2">
           Leia os termos
@@ -183,7 +91,7 @@ export const Form = () => {
       </div>
       <button
         type="submit"
-        className="bg-purple-600 hover:bg-purple-500 font-medium text-sm py-2 px-4 rounded-lg text-white"
+        className="bg-slate-600 hover:bg-slate-500 font-medium text-sm py-2 px-4 rounded-lg text-white"
       >
         Cadastrar
       </button>
--- src/types/User.ts
+++ src/types/User.ts
@@ -1,9 +1,5 @@
 export type User = {
   name?: string;
-  lastname?: string;
   email?: string;
-  gender?: string;
-  password?: string;
-  confirmPassword?: string;
   agree?: boolean | string;
 };
